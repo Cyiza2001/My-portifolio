05bbfa2f58faf049c763539d121c74d6de826eec
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-02-26 10:58:15 +0200

    another commit

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////SELECT ELEMENTS///////////////////////////////
 const menuIcon = document.querySelector(".menu-img");
+const crossIcon = document.querySelector(".cross-img");
 const header = document.querySelector("header");
+
+////////////HIDE THE CROSS ICON//////
+crossIcon.style.display = "none";
 
 //////////////////////////////////////////////////////////////
 /////////////////EVENT LISTENERS /////////////////////////
 
-//By clicking on the Menu icon
-menuIcon.style.color = "red";
+//By clicking on the Menu ico
 menuIcon.addEventListener("click", function () {
-  //   e.preventDefault();
-  console.log(menuIcon);
-  console.log("kabaye");
   header.style.opacity = 1;
+  menuIcon.style.display = "none";
+  crossIcon.style.display = "block";
+});
+crossIcon.addEventListener("click", function () {
+  header.style.opacity = 0;
+  crossIcon.style.display = "none";
+  menuIcon.style.display = "block";
 });
